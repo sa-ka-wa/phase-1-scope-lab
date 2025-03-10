48a19f45f1cef72b1a9c263a492353c8433c1e37
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-10 14:26:59 +0300

    scopes

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,42 +1,61 @@
-require ( './helpers.js' );
+require("./helpers.js");
 
-describe('Fix the Scope', function() {
-  describe('declare customerName to be bob in global scope', function() {
-    it('returns the customerName', function() {
-      expect(window.customerName).to.equal('bob');
+var customerName = "bob";
+
+describe("Fix the Scope", function () {
+  describe("declare customerName to be bob in global scope", function () {
+    it("returns the customerName", function () {
+      expect(window.customerName).to.equal("bob");
     });
   });
 
-  describe('upperCaseCustomerName()', function() {
-    it('modifies the customerName variable', function() {
-      expect(window.customerName).to.equal('bob');
+  function upperCaseCustomerName() {
+    return (customerName = customerName.toUpperCase());
+  }
+
+  describe("upperCaseCustomerName()", function () {
+    it("modifies the customerName variable", function () {
+      expect(window.customerName).to.equal("bob");
 
       upperCaseCustomerName();
 
-      expect(window.customerName).to.equal('BOB');
+      expect(window.customerName).to.equal("BOB");
     });
   });
-  describe('setBestCustomer()', function() {
-    it('setBestCustomer', function() {
+
+  function setBestCustomer() {
+    bestCustomer = "not bob";
+  }
+
+  describe("setBestCustomer()", function () {
+    it("setBestCustomer", function () {
       expect(window.bestCustomer).to.equal(undefined);
 
       setBestCustomer();
 
-      expect(window.bestCustomer).to.equal('not bob');
+      expect(window.bestCustomer).to.equal("not bob");
     });
   });
 
-  describe('overwriteBestCustomer()', function() {
-    it('overwrites the best customer', function() {
-      overwriteBestCustomer('maybe bob');
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
 
-      expect(window.bestCustomer).to.equal('maybe bob');
+  describe("overwriteBestCustomer()", function () {
+    it("overwrites the best customer", function () {
+      overwriteBestCustomer("maybe bob");
+
+      expect(window.bestCustomer).to.equal("maybe bob");
     });
   });
 
-  describe('changeLeastFavoriteCustomer()', function() {
-    it('unsuccessfully tries to reassign the least favorite customer', function() {
-      expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+  const leastFavoriteCustomer = "bob";
+
+  describe("changeLeastFavoriteCustomer()", function () {
+    it("unsuccessfully tries to reassign the least favorite customer", function () {
+      expect(changeLeastFavoriteCustomer).to.throw(
+        "Assignment to constant variable."
+      );
     });
   });
 });
